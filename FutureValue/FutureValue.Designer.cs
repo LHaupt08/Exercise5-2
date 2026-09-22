abd3d91422cbde60a6e6a64cfa3fbe7caf0836db
LHaupt1s <LHaupt1s@capetigers.com>
2026-09-22 14:26:28 -0500

    final commit

--- FutureValue/FutureValue.Designer.cs
+++ FutureValue/FutureValue.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FutureValue
 {
-    partial class Form1
+    partial class FutureValue
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtMonthlyInvestment = new TextBox();
+            txtInterestRate = new TextBox();
+            txtYears = new TextBox();
+            txtFutureValue = new TextBox();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Monthly Investment:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Yearly Interest Rate:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Number of Years:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(28, 95);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Future Value:";
+            // 
+            // txtMonthlyInvestment
+            // 
+            txtMonthlyInvestment.Location = new Point(153, 6);
+            txtMonthlyInvestment.Name = "txtMonthlyInvestment";
+            txtMonthlyInvestment.Size = new Size(100, 23);
+            txtMonthlyInvestment.TabIndex = 4;
+            // 
+            // txtInterestRate
+            // 
+            txtInterestRate.Location = new Point(153, 35);
+            txtInterestRate.Name = "txtInterestRate";
+            txtInterestRate.Size = new Size(100, 23);
+            txtInterestRate.TabIndex = 5;
+            // 
+            // txtYears
+            // 
+            txtYears.Location = new Point(153, 64);
+            txtYears.Name = "txtYears";
+            txtYears.Size = new Size(100, 23);
+            txtYears.TabIndex = 6;
+            // 
+            // txtFutureValue
+            // 
+            txtFutureValue.Location = new Point(153, 92);
+            txtFutureValue.Name = "txtFutureValue";
+            txtFutureValue.ReadOnly = true;
+            txtFutureValue.Size = new Size(100, 23);
+            txtFutureValue.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(85, 127);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(75, 23);
+            btnCalculate.TabIndex = 8;
+            btnCalculate.Text = "&Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(178, 127);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 9;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // FutureValue
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(275, 160);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtFutureValue);
+            Controls.Add(txtYears);
+            Controls.Add(txtInterestRate);
+            Controls.Add(txtMonthlyInvestment);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FutureValue";
+            Text = "Future Value";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtMonthlyInvestment;
+        private TextBox txtInterestRate;
+        private TextBox txtYears;
+        private TextBox txtFutureValue;
+        private Button btnCalculate;
+        private Button btnExit;
     }
 }
--- FutureValue/FutureValue.cs
+++ FutureValue/FutureValue.cs
@@ -1,10 +1,36 @@
 namespace FutureValue
 {
-    public partial class Form1 : Form
+    public partial class FutureValue : Form
     {
-        public Form1()
+        public FutureValue()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+            decimal yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
+            int years = Convert.ToInt32(txtYears.Text);
+
+            int months = years * 12;
+            decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+
+            decimal futureValue = 0m;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+            }
+
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
